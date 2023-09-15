8b15e2ff506867f012b49a6d080f92313fd872c9
New User NameStyematic <taiddse173059@fpt.edu.vn>
2023-09-16 00:56:43 +0700

    add html

---
+++ Torikago-Shop-main/Torikago-Shop/src/main/java/com/group3/torikago/Torikago/Shop/model/Role.java
@@ -0,0 +1,24 @@
+package com.group3.torikago.Torikago.Shop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String roleName;
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users = new ArrayList<>();
+}
