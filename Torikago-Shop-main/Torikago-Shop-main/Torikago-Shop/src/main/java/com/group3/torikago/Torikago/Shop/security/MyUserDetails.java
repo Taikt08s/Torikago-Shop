6d9f93c7b431525388fd5ee36e0f048b8022b7ae
Styematic <taiddse173059@fpt.edu.vn>
2023-09-16 17:06:32 +0700

    fix bug

---
+++ Torikago-Shop-main/Torikago-Shop-main/Torikago-Shop/src/main/java/com/group3/torikago/Torikago/Shop/security/MyUserDetails.java
@@ -0,0 +1,60 @@
+package com.group3.torikago.Torikago.Shop.security;
+
+import com.group3.torikago.Torikago.Shop.model.Role;
+import com.group3.torikago.Torikago.Shop.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class MyUserDetails implements UserDetails {
+    private User user;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<Role> roles = user.getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ Torikago-Shop-main/Torikago-Shop-main/Torikago-Shop/src/test/java/com/group3/torikago/Torikago/Shop/TorikagoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.torikago.Torikago.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorikagoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
