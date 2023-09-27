2e8d822599b872dd5d6d5554076a1bdb4e8f1bdb
tuankiet12012003 <kietdhtse172938@fpt.edu.vn>
2023-09-27 22:06:51 +0700

    update AdminController

--- Torikago-Shop/src/main/java/com/group3/torikago/Torikago/Shop/controller/AdminController.java
+++ Torikago-Shop/src/main/java/com/group3/torikago/Torikago/Shop/controller/AdminController.java
@@ -1,9 +1,12 @@
 package com.group3.torikago.Torikago.Shop.controller;
 
+import com.group3.torikago.Torikago.Shop.dto.AccessoryDTO;
 import com.group3.torikago.Torikago.Shop.dto.BirdCageDTO;
 import com.group3.torikago.Torikago.Shop.dto.ProductDTO;
+import com.group3.torikago.Torikago.Shop.model.AccessoryDetail;
 import com.group3.torikago.Torikago.Shop.model.BirdCageDetail;
 import com.group3.torikago.Torikago.Shop.model.Product;
+import com.group3.torikago.Torikago.Shop.service.AccessoryService;
 import com.group3.torikago.Torikago.Shop.service.BirdCageService;
 import com.group3.torikago.Torikago.Shop.service.ProductService;
 import jakarta.annotation.security.RolesAllowed;
@@ -31,13 +34,14 @@ public class AdminController {
     }
 
     private ProductService productService;
-
     private BirdCageService birdCageService;
+    private AccessoryService accessoryService;
 
     @Autowired
-    public AdminController(ProductService productService, BirdCageService birdCageService) {
+    public AdminController(ProductService productService, BirdCageService birdCageService, AccessoryService accessoryService) {
         this.productService = productService;
         this.birdCageService = birdCageService;
+        this.accessoryService = accessoryService;
     }
 
     @GetMapping("/admin/product-table")
@@ -47,6 +51,7 @@ public class AdminController {
         model.addAttribute("products", products);
         return "admin-product";
     }
+
 
     //    @GetMapping("/admin/product-table/bird-cage/add")
 //    @RolesAllowed({"ADMIN"})
@@ -69,7 +74,6 @@ public class AdminController {
         model.addAttribute("birdCageDetail", birdCageDetail);
         return "bird-cage";
     }
-
     @PostMapping("/admin/product-table/bird-cage/add")
     public String saveBirdCage(@ModelAttribute("birdCageDetail") @Valid BirdCageDTO birdCageDTO, BindingResult result,
                                @ModelAttribute("product") @Valid ProductDTO productDTO,BindingResult birdCageBindingResult,
@@ -81,6 +85,29 @@ public class AdminController {
         productDTO.setProductType("bird cage");
         productDTO.setUnitsOnOrder(0);
         birdCageService.saveBirdCage(birdCageDTO, productDTO);
+        return "redirect:/admin";
+    }
+    @GetMapping("/admin/product-table/accessory/add")
+    @RolesAllowed({"ADMIN"})
+    public String addAccessory(Model model) {
+        AccessoryDetail accessoryDetail = new AccessoryDetail();
+        Product product = new Product();
+        model.addAttribute("product", product);
+        model.addAttribute("accessoryDetail", accessoryDetail);
+        return "accessory";
+    }
+
+    @PostMapping("/admin/product-table/accessory/add")
+    public String saveAccessory(@ModelAttribute("accessoryDetail") @Valid AccessoryDTO accessoryDTO, BindingResult result,
+                                @ModelAttribute("product") @Valid ProductDTO productDTO, BindingResult accessoryBindingResult,
+                                BindingResult productBindingResult) {
+        if (accessoryBindingResult.hasErrors() == productBindingResult.hasErrors()) {
+            // If there are validation errors, return to the form page with errors
+            return "accessory";
+        }
+        productDTO.setProductType("accessory");
+        productDTO.setUnitsOnOrder(0);
+        accessoryService.saveAccessory(accessoryDTO, productDTO);
         return "redirect:/admin";
     }
 //    @GetMapping("/admin/product-table/{productID}/edit")
