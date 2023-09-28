c549e7af7010f818c023b616fbae68b4c67f530b
Taikt08s <styematic@gmail.com>
2023-09-28 23:27:05 +0700

    add pagination and sorting

--- Torikago-Shop/src/main/java/com/group3/torikago/Torikago/Shop/repository/ProductRepository.java
+++ Torikago-Shop/src/main/java/com/group3/torikago/Torikago/Shop/repository/ProductRepository.java
@@ -2,7 +2,8 @@ package com.group3.torikago.Torikago.Shop.repository;
 
 import com.group3.torikago.Torikago.Shop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends PagingAndSortingRepository<Product,Long> {
 
 }
