683b75fcef6173d4fbe3595c209fff3c1d53c0ba
Taikt08s <styematic@gmail.com>
2023-10-25 09:45:09 +0700

    UI check

--- src/main/java/com/group3/torikago/Torikago/Shop/repository/ProductRepository.java
+++ src/main/java/com/group3/torikago/Torikago/Shop/repository/ProductRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ProductRepository extends PagingAndSortingRepository<Product,Long> {
+import java.util.UUID;
+
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
     @Query("SELECT p FROM Product p WHERE p.productName LIKE %?1%")
     Page<Product> findAll(String keyword, Pageable pageable);
 
